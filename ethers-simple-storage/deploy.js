a2ae7d135362d72743e2049c8115f1707bd555b3
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 18:36:06 +0200

    ROkas

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -5,15 +5,14 @@ require("dotenv").config()
 async function main () {
     // compile the contract
     // compile the contract separately
-    // http://127.0.0.1:7545
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    // const wallet = new ethers.Wallet(
-    //     process.env.PRIVATE_KEY,
-    //     provider
-    // );
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
-    wallet = await wallet.connect(provider);
+    const wallet = new ethers.Wallet(
+        process.env.PRIVATE_KEY,
+        provider
+    );
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    // wallet = await wallet.connect(provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
 
