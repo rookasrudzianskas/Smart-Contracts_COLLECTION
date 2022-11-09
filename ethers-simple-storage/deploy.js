b10754437261c9683f094ab8a3fc9d777180bf90
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 16:35:54 +0200

    Add deploy.js
    This script will deploy the contract to the network.

---
+++ ethers-simple-storage/deploy.js
@@ -0,0 +1,30 @@
+
+
+async function main () {
+    function hi() {
+        console.log('hi');
+    }
+    hi();
+
+}
+main();
+
+
+// synchronous [solidity]
+// asynchronous [javascript]
+
+// cooking
+// Synchronous
+// 1. Put popcorn in microwave -> Promise
+// 2. Wait for popcorn to finish
+// 3. Pour drinks for everyone
+
+// Asynchronous
+// 1. Put popcorn in the mircrowave
+// 2. Pour drinks for everyone
+// 3. Wait for popcorn to finish
+
+// Promise
+// Pending
+// Fulfilled
+// Rejected
