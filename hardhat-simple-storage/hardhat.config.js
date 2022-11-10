6eb2c08c8e651bc818c953d297fae8ec29cf0af0
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 09:11:51 +0200

    Rokas

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    }
+  },
   solidity: "0.8.8",
 };
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,7 +1,13 @@
 const { ethers, run, network } = require("hardhat");
 
 async function main() {
-
+      const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+      console.log("Deploying contract... 🚀");
+      const simpleStorage = await SimpleStorageFactory.deploy();
+      await simpleStorage.deployed();
+      // What is the private key of the account that deployed the contract?
+      // What is the rpc endpoint of the network that the contract is deployed to?
+      console.log(`Contract deployed to ${simpleStorage.address} 🚀`);
 }
 
 main()
