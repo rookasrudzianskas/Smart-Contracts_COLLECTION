15e106b72340c989b1a0c8aac477844536aa0f27
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 12:05:41 +0200

    Rokas

---
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -0,0 +1,9 @@
+const hre = require("hardhat");
+
+// module.exports = async function main(hre) {
+//     console.log("deploying flashtoken!!");
+// }
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    console.log("deploying flashtoken!!");
+}
--- hardhat-fund-me/hardhat.config.js
+++ hardhat-fund-me/hardhat.config.js
@@ -1,5 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
+// "ethereum-waffle": "^3.4.0",
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
