0772d1eac3d3f902edc2836707fe0fc36330b4a4
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 18:00:13 +0200

    ROkas

--- interface-fund-me/index.js
+++ interface-fund-me/index.js
@@ -1,4 +1,5 @@
 import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
