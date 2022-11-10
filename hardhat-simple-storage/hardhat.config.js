d7e12538d897b160c2cc474d230119395bac7b09
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 10:06:16 +0200

    feat: add block-number task
    This task prints the current block number.

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
--- hardhat-simple-storage/tasks/block-number.js
+++ hardhat-simple-storage/tasks/block-number.js
@@ -1,3 +1,13 @@
 const { task } = require("hardhat/config");
 
+task("block-number", "Prints the current block number").setAction(
+    // const blockTask = async function() => {}
+    // async function blockTask() {}
+    // hre - hardhat environment
+    async (taskArgs, hre) => {
+        const blockNumber = await hre.ethers.provider.getBlockNumber();
+        console.log(`Current block number: ${blockNumber} 🔴`);
+    }
+)
 
+module.exports = {};
