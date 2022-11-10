f043f796a8945644575c20cb3ed2ccfe7933412f
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 17:35:05 +0200

    Rokas"

---
+++ interface-fund-me/index.js
@@ -0,0 +1,22 @@
+
+const connectButton = document.getElementById("connectButton");
+connectButton.onclick = connect;
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
+    }
+}
+
+async function fund(ethAmount) {
+    console.log(`Funding ${ethAmount} ETH`);
+}
