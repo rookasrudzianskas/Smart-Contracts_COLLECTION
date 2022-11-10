efce2a3b0a0fa426896923c808f43d69a30208a7
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 10:43:11 +0200

    feat: add a test to check the initial value of the contract
    This test checks that the initial value of the contract is 0.

--- hardhat-simple-storage/test/test-deploy.js
+++ hardhat-simple-storage/test/test-deploy.js
@@ -3,15 +3,19 @@ const { expect, assert } = require("chai");
 
 describe("SimpleStorage", function () {
     let simpleStorageFactory, simpleStorage;
+    // This beforeEach, defines what the test should do
     beforeEach(async function () {
         simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
         simpleStorage = await simpleStorageFactory.deploy();
     })
 
+    // and this it, defines what the test should expect
     it("Should start with a favorite number of 0", async function () {
         const currentValue = await simpleStorage.retrieve();
-        const expectedValue = 0;
+        const expectedValue = "0";
         // assert or expect
+        // and asset checks the test output if it is true or false
+        assert.equal(currentValue.toString(), expectedValue);
     })
 })
 // describe("SimpleStorage", () => {})
