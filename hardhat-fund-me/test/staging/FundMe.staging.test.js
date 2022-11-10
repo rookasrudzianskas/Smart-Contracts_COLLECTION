143aee948379b558095731253af90f24cdafcd6e
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 16:03:35 +0200

    Rokas

--- hardhat-fund-me/test/staging/FundMe.staging.test.js
+++ hardhat-fund-me/test/staging/FundMe.staging.test.js
@@ -1,3 +1,17 @@
 const { assert } = require("chai");
 const { network, ethers, getNamedAccounts } = require("hardhat");
 const { developmentChains } = require("../../helper-hardhat-config");
+
+developmentChains.includes(network.name) ? describe.skip : describe("FundMe", function () {
+    let fundMe;
+    let deployer;
+    const sendValue = ethers.utils.parseEther("1");
+    beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        fundMe = await ethers.getContract("FundMe", deployer);
+    });
+
+    it("allows people to fund and withdraw", async function () {
+
+    });
+    });
