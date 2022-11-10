7ab1051c7a33c21e89f148744a8c0edcc4c4f189
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 18:26:49 +0200

    Rokas

--- interface-fund-me/index.js
+++ interface-fund-me/index.js
@@ -31,9 +31,25 @@ async function fund() {
         try {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount),
-            })
+            });
+            // listen for the tx to be mined
+            await listenForTransactionMine(transactionResponse, provider);
         } catch (error) {
             console.log(error);
         }
     }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`);
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(`Completed ${transactionReceipt.confirmations} confirmations`);
+                resolve();
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    });
 }
