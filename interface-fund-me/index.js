5326afb73c7f702fd501651ff1fae68b421ee739
Rokas <rookas.rudzenskas@gmail.com>
2022-11-10 17:38:10 +0200

    Rokas

--- interface-fund-me/index.js
+++ interface-fund-me/index.js
@@ -1,6 +1,9 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
 
 const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
 connectButton.onclick = connect;
+fundButton.onclick = fund;
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -19,4 +22,7 @@ async function connect() {
 
 async function fund(ethAmount) {
     console.log(`Funding ${ethAmount} ETH`);
+    if(typeof window.ethereum !== "undefined") {
+
+    }
 }
