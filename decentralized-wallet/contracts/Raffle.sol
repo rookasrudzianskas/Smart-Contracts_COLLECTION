9d5c98aa16df939abdc291eada1d81814566e60f
Rokas <rookas.rudzenskas@gmail.com>
2022-11-11 09:07:48 +0200

    Add a new feature
    This is a new feature that I have been working on.

--- decentralized-wallet/contracts/Raffle.sol
+++ decentralized-wallet/contracts/Raffle.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
+
+// Enter the lottery by sending some ETH to this contract.
+// Pic a random winner
+// Winner to be selected every x minutes -> completely automated
+// Chainlink Oracle -> Randomness, Automated execution (Chainlink Keepers);
+// Chainlink VRF -> Randomness
+
