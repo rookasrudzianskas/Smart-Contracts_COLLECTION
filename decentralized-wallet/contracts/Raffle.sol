32c228054db2107ad5530c358005f8c820fbcd15
Rokas <rookas.rudzenskas@gmail.com>
2022-11-11 12:13:46 +0200

    Rokas

--- decentralized-wallet/contracts/Raffle.sol
+++ decentralized-wallet/contracts/Raffle.sol
@@ -11,19 +11,29 @@ pragma solidity ^0.8.7;
 error Raffle_NotEnoughETHEntered();
 
 contract Raffle {
+    // state variables
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() {
+    function enterRaffle() public payable {
         // require msg.value > i_entranceFee
         if(msg.value < i_entranceFee) { revert Raffle_NotEnoughETHEntered(); }
+        // we keep track of all players
+        s_players.push(payable(msg.sender));
+        // Events
+
     }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
 
