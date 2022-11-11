161a7f9c9441bdb40d96635fca7b5fd58a7d8a63
Rokas <rookas.rudzenskas@gmail.com>
2022-11-11 09:25:02 +0200

    Add Raffle contract
    This contract allows users to enter a raffle by sending a certain amount of ETH.

--- decentralized-wallet/contracts/Raffle.sol
+++ decentralized-wallet/contracts/Raffle.sol
@@ -8,3 +8,23 @@ pragma solidity ^0.8.7;
 // Chainlink Oracle -> Randomness, Automated execution (Chainlink Keepers);
 // Chainlink VRF -> Randomness
 
+error Raffle_NotEnoughETHEntered();
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() {
+        // require msg.value > i_entranceFee
+        if(msg.value < i_entranceFee) { revert Raffle_NotEnoughETHEntered(); }
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
+
+// function pickRandomWinner() {}
