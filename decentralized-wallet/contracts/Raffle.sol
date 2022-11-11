b7e938799c0b40a223fa79a0f75c74a29f8d1d28
Rokas <rookas.rudzenskas@gmail.com>
2022-11-11 12:50:38 +0200

    Rokas

--- decentralized-wallet/contracts/Raffle.sol
+++ decentralized-wallet/contracts/Raffle.sol
@@ -8,18 +8,33 @@ pragma solidity ^0.8.7;
 // Chainlink Oracle -> Randomness, Automated execution (Chainlink Keepers);
 // Chainlink VRF -> Randomness
 
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
+
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // state variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
     //Events
-    event.RaffleEnter(address indexed player);
+    event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
     }
 
     function enterRaffle() public payable {
@@ -31,10 +46,20 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    function pickRandomWinner() {
+    function requestRandomWinner() external {
+        // Request a random number
+        // Once we do, do something with it
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+        )
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
 
     }
 
+    // View pure functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
