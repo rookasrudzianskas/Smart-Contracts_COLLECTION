9d3dfbab61add85af45c1e921c7bd29227b0ceec
Rokas <rookas.rudzenskas@gmail.com>
2022-11-13 20:49:08 +0200

    Rokas

---
+++ smartcontract-lottery/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <div className="">
+            <Head>
+                <title>Web3 Lottery</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+        </div>
+    )
+}
+
+export default Home
