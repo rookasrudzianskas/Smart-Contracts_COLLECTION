53736d05a0ca86e5052dbcf9b20604c2a040c976
Rokas <rookas.rudzenskas@gmail.com>
2022-11-14 08:57:41 +0200

    Rokas

--- smartcontract-lottery/components/ManualHeader.js
+++ smartcontract-lottery/components/ManualHeader.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {MoralisProvider, useMoralis} from "react-moralis";
 
 const ManualHeader = ({}) => {
-    const {enableWeb3} = useMoralis();
+    const {enableWeb3, account} = useMoralis();
 
     return (
         <div className="flex justify-between max-w-7xl mx-auto p-5 items-center">
             <p className="text-xl md:text-2xl lg:text-3xl font-bold">Decentralized Lottery</p>
             <div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-150">
-                    Connect Wallet
-                </button>
+                {account ? (
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-150">
+                        {account.slice(0, 5)}...{account.slice(-4)}
+                    </button>
+                ) : (
+                    <button onClick={async () => await enableWeb3()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-150">
+                        Connect Wallet
+                    </button>
+                )}
             </div>
         </div>
     );
