c05c5b16556a7312dca39a8b6e7fbb40bcc4e7bf
Rokas <rookas.rudzenskas@gmail.com>
2022-11-14 15:49:21 +0200

    initial

---
+++ hardhat-erc20-token/helper-hardhat-config.js
@@ -0,0 +1,19 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+    },
+    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+    5: {
+        name: "goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+}
+const INITIAL_SUPPLY = "1000000000000000000000000"
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    INITIAL_SUPPLY,
+}
