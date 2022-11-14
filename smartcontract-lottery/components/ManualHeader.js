625f97a44c60ee74c966f7ba894035c3d66f9770
Rokas <rookas.rudzenskas@gmail.com>
2022-11-14 08:54:53 +0200

    Rokas

--- smartcontract-lottery/components/ManualHeader.js
+++ smartcontract-lottery/components/ManualHeader.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { MoralisProvider } from "react-moralis";
+import {MoralisProvider, useMoralis} from "react-moralis";
 
 const ManualHeader = ({}) => {
+    const {enableWeb3} = useMoralis();
+
     return (
         <div className="flex justify-between max-w-7xl mx-auto p-5 items-center">
             <p className="text-xl md:text-2xl lg:text-3xl font-bold">Decentralized Lottery</p>
