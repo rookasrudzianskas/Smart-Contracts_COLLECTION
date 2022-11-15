180e71b437046ee612a7d2165e13e4e8a77a0318
Rokas <rookas.rudzenskas@gmail.com>
2022-11-15 17:18:16 +0200

    Add a new breed of dog to the NFT
    This commit adds a new breed of dog to the NFT.
    
    The new breed is a St. Bernard.
    
    The St. Bernard has a 60% chance of being minted.
    
    The Shiba Inu has a 30% chance of being minted.
    
    The Pug has a 10% chance of being minted.

--- hardhat-nft/contracts/ RandomIpfsNft.sol
+++ hardhat-nft/contracts/ RandomIpfsNft.sol
@@ -1,18 +1,115 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract RandomIpfsNft {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 
-    function requestNft() public {
+error RandomIpfsNft__AlreadyInitialized();
+error RandomIpfsNft__NeedMoreETHSent();
+error RandomIpfsNft__RangeOutOfBounds();
+error RandomIpfsNft__TransferFailed();
 
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+
+    // Type declaration
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
+    // VRF helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // NFT Variables
+    uint256 public  s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_dogTokenUris;
+    uint256 internal i_mintFee;
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 mintFee,
+        uint32 callbackGasLimit,
+        string[3] memory dogTokenUris
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_dogTokenUris = dogTokenUris;
+        i_mintFee = mintFee;
     }
 
-    function tokenURI(uint256) public {
+
+    function requestNft() public payable returns(uint256 requestId) {
+        if(msg.value < i_mintFee) {
+            revert RandomIpfsNft__NeedMoreETHSent();
+        }
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+        // What does this token look like
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+        // We will get number between 0 - 99
+        // 7 -> Pug
+        // 88 -> St. Bernard
+        // 45 -> St. Bernard
+        // 12 -> Shiba Inu
+        Breed dogBreed = getBreedFromModdedRng(moddedRng);
+        _safeMint(dogOwner, newTokenId);
+        _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool, success)
+    }
+
+    function getBreedFromModdedRng(uint256 moddedRng) public pure returns (Breed) {
+        uint256 cumulativeSum = 0;
+        uint256[3] memory chancesArray = getChanceArray();
+        // moddedRng = 25
+        // i = 0;
+        // cumulativeSum = 0;
+        for(uint256 i = 0; i < chancesArray.length; i++) {
+            if(moddedRng >= cumulativeSum && moddedRng < cumulativeSum + chancesArray[i]) {
+                return Breed(i);
+            }
+            cumulativeSum += chancesArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns(uint256[3] memory) {
+        // index 0 has 10% and index 1 has 20% and index 2 has 60%
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
+
+    function tokenURI(uint256) public view override returns (string memory) {
 
     }
 }
