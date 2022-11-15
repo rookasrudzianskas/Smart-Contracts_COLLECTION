db42307984b7fbc6aeb7443786073afb42626114
Rokas <rookas.rudzenskas@gmail.com>
2022-11-15 17:32:11 +0200

    Update hardhat-play submodule
    This commit updates the hardhat-play submodule to the latest commit.
    
    The hardhat-play submodule is used to run the Hardhat Playground.
    
    The Hardhat Playground is a web-based IDE that allows you to write, compile,
    deploy and interact with Solidity smart contracts.
    
    The Hardhat Playground is available at https://play.hardhat.org.

--- hardhat-nft/deploy/02-deploy-random-ipfs-nft.js
+++ hardhat-nft/deploy/02-deploy-random-ipfs-nft.js
@@ -1,13 +1,25 @@
-const { network } = require("hardhat")
+const { network, ethers} = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 const fs = require("fs")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log } = deployments
-    const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
-    let ethUsdPriceFeedAddress
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let ethUsdPriceFeedAddress, vrfCoordinatorV2Address, subscriptionId;
+
+    if(developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+        const tx = await vrfCoordinatorV2Mock.createSubscription();
+        const txReceipt = await tx.wait(1);
+        subscriptionId = txReceipt.events[0].args.subscriptionId;
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
+        subscriptionId = networkConfig[chainId].subscriptionId;
+
+    }
 
     // if (chainId == 31337) {
     //     // Find ETH/USD price feed
--- hardhat-nft/helper-hardhat-config.js
+++ hardhat-nft/helper-hardhat-config.js
@@ -14,7 +14,7 @@ const networkConfig = {
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000", // 500,000 gas
         mintFee: "10000000000000000", // 0.01 ETH
-        subscriptionId: "1002", // add your ID here!
+        subscriptionId: "6375", // 6375
     },
 }
 
