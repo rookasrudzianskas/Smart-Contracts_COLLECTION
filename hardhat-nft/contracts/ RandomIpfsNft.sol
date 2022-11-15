cc386b0237a4d6af54c4c13cc96ce8411ec6fa24
Rokas <rookas.rudzenskas@gmail.com>
2022-11-15 13:10:48 +0200

    Add a function to request a new NFT
    This function will be called by the frontend to request a new NFT.
    
    It will be fulfilled by the `fulfillRandomWords` function.

--- hardhat-nft/contracts/ RandomIpfsNft.sol
+++ hardhat-nft/contracts/ RandomIpfsNft.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.8;
 
 contract RandomIpfsNft {
 
+
+    function requestNft() public {
+
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {
+
+    }
+
+    function tokenURI(uint256) public {
+
+    }
 }
