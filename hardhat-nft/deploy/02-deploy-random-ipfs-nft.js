4a42f926dd296853d1d2542236042754f165ea43
Rokas <rookas.rudzenskas@gmail.com>
2022-11-15 21:04:29 +0200

    feat: add Pinata upload to token URI metadata
    This commit adds the ability to upload the token URI metadata to Pinata.
    
    The metadata is stored in IPFS and the IPFS hash is returned.
    
    The IPFS hash is then used as the token URI.

--- hardhat-nft/deploy/02-deploy-random-ipfs-nft.js
+++ hardhat-nft/deploy/02-deploy-random-ipfs-nft.js
@@ -2,9 +2,22 @@ const { network, ethers} = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 const fs = require("fs")
-const {storeImages} = require("../utils/uploadToPinata");
+const {storeImages, storeTokenMetadata, storeTokenUriMetadata} = require("../utils/uploadToPinata");
+require("dotenv").config();
 
 const imagesLocation = "./images/randomNft";
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100
+        },
+    ],
+};
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -13,8 +26,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     let ethUsdPriceFeedAddress, vrfCoordinatorV2Address, subscriptionId, tokenUris;
 
     // get the ipfs hashes for the images
-    if(process.env.UPLOAD_TO_PINATA = "true") {
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        console.log("Uploading images to Pinata... 🚀");
         tokenUris = await handleTokenUris();
+        console.log("Images uploaded to Pinata! ✅");
     }
     // with our own IPFS node or programmatically
 
@@ -31,7 +46,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
 
     log("----------------------------------------------------");
-    await storeImages(imagesLocation);
 
     // const args = [
     //     vrfCoordinatorV2Address,
@@ -44,16 +58,33 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     async function handleTokenUris() {
         tokenUris = [];
         // store the image in IPFS and store data in IPFS
-
+        const { responses: imageUploadResponses, files } = await storeImages(imagesLocation);
+        for (let imageUploadResponseIndex in imageUploadResponses) {
+            // create metadata
+            // upload metadata to IPFS
+            let tokenUriMetadata = { ...metadataTemplate };
+            // pug.png, pug.jpg, pug.jpeg
+            tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "");
+            tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`;
+            tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`;
+            console.log("FORMING METADATA FOR TOKEN URI");
+            // console.log(`tokenUriMetadata: ${JSON.stringify(tokenUriMetadata, null, 4)}`);
+            // store the file JSON in IPFS
+            const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata);
+            // We will have the array of IPFS hashes, that points to the metadata of each token
+            tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`);
+        }
+        console.log("Token URIs Uploaded! They are:");
+        console.log(tokenUris);
         return tokenUris;
     }
 
 
     // Verify the deployment
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying...")
-        await verify(dynamicSvgNft.address, arguments)
-    }
+    // if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    //     log("Verifying...")
+    //     await verify(dynamicSvgNft.address, arguments)
+    // }
 }
 
 module.exports.tags = ["all", "randomipfs", "main"];
