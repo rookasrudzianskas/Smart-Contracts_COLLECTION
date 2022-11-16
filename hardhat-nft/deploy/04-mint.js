f58fb822ad3da2961bbb2697cba6ace7bff1b7cb
Rokas <rookas.rudzenskas@gmail.com>
2022-11-16 15:47:23 +0200

    Add a new NFT contract
    This commit adds a new NFT contract to the project.
    
    The new contract is called DynamicSvgNft and it is a simple NFT contract
    that allows the owner to change the tokenURI of the NFT.
    
    This is a very simple contract that is meant to be used as an example
    of how to create a new NFT contract.

--- hardhat-nft/deploy/04-mint.js
+++ hardhat-nft/deploy/04-mint.js
@@ -1,4 +1,5 @@
 const { network, ethers } = require("hardhat");
+const {developmentChains} = require("../helper-hardhat-config");
 
 
 module.exports = async ({ getNamedAccounts }) => {
@@ -16,12 +17,26 @@ module.exports = async ({ getNamedAccounts }) => {
     const mintFee = await randomIpfsNft.getMintFee();
 
     await new Promise(async (resolve, reject) => {
-
-    }
-
+        setTimeout(resolve, 1500000) // 15 minutes
+        randomIpfsNft.once('NftMinted', async function () {
+            resolve();
+        })
         const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() });
-    await randomIpfsNftMintTx
+        const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1);
+        if(developmentChains.includes(network.name)) {
+            const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString();
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address);
+        }
+    });
 
+    console.log(`Random IPFS NFT index 0 has tokenURI: ${await randomIpfsNft.tokenURI(0)}`);
+    // Dynamic SVG NFT
+    const highValue = ethers.utils.parseEther("40000");
+    const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer);
+    const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue.toString());
+    await dynamicSvgNftMintTx.wait(1);
+    console.log(`Dynamic SVG NFT index 0 has tokenURI: ${await dynamicSvgNft.tokenURI(0)}`);
 }
 
 module.exports.tags = ["all", "mint"];
