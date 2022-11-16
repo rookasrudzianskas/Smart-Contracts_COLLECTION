1d931f62b940f9467c130962e101cd1438730267
Rokas <rookas.rudzenskas@gmail.com>
2022-11-16 11:07:39 +0200

    Add DynamicSvgNft contract
    This contract will be used to create NFTs that have dynamic SVG images.
    
    The images will be generated by a Chainlink oracle that will query the
    price of the token and return a SVG image with the price.

---
+++ hardhat-nft/contracts/ DynamicSvgNft.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "base64-sol/base64.sol";
+import "hardhat/console.sol";
+
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
+
+contract DynamicSvgNft is ERC721, Ownable {
+    uint256 private s_tokenCounter;
+    string private s_lowImageURI;
+    string private s_highImageURI;
+
+
+}
