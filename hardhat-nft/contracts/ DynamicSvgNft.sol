3c2338f7f9837e5db4a1e6fa3c525bd6107c6955
Rokas <rookas.rudzenskas@gmail.com>
2022-11-16 11:23:08 +0200

    Add a function to convert SVG to base64 encoded image URI
    This will allow us to store SVG data in our contract, and then
    convert it to a base64 encoded image URI that can be used in
    our frontend.

--- hardhat-nft/contracts/ DynamicSvgNft.sol
+++ hardhat-nft/contracts/ DynamicSvgNft.sol
@@ -11,8 +11,28 @@ error ERC721Metadata__URI_QueryFor_NonExistentToken();
 
 contract DynamicSvgNft is ERC721, Ownable {
     uint256 private s_tokenCounter;
-    string private s_lowImageURI;
-    string private s_highImageURI;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+
+    // mint
+    // store our SVG information somewhere
+    // some login to show X image or to show Y image
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns(string memory) {
+        // Will take any svg, and spit out a base64 encoded image URI
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
 
 }
