4c65126c95915681b32f7ea7c323302a585aab57
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 14:00:01 +0200

    Add a new NFT to the marketplace
    This commit adds a new NFT to the marketplace.
    
    The NFT is a picture of a cat.

--- nft-marketplace/pages/_app.tsx
+++ nft-marketplace/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {MoralisProvider} from "react-moralis";
+import Head from "next/head";
 
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <MoralisProvider initializeOnMount={false}>
+    return (
+      // @ts-ignore
+      <MoralisProvider
+          appId={APP_ID}
+          serverUrl={SERVER_URL}
+      >
+          <Head>
+              <title>NFT Marketplace</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
         <Component {...pageProps} />
       </MoralisProvider>
   )
--- nft-marketplace/pages/index.tsx
+++ nft-marketplace/pages/index.tsx
@@ -6,11 +6,6 @@ import Header from '../components/Header';
 const Home: NextPage = () => {
   return (
     <div className="max-w-7xl mx-auto">
-      <Head>
-        <title>NFT Marketplace</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Header />
     </div>
   )
