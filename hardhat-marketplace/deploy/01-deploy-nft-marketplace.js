df5bad68034440cebdcf60dc2d4275e74daee0ed
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 09:37:17 +0200

    feat: add verification script
    This commit adds a verification script to the project.
    
    The script is used to verify the contract on Etherscan.
    
    The script is run after the contract is deployed.
    
    The script is run only on the mainnet.

---
+++ hardhat-marketplace/deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,4 @@
+const { network } = require("hardhat");
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
