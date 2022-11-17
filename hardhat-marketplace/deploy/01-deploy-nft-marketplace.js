ef04868361d052962f256b9a93c8b08f139d07e1
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 10:23:14 +0200

    feat: add a new feature
    This is a new feature that I'm adding to the project.

--- hardhat-marketplace/deploy/01-deploy-nft-marketplace.js
+++ hardhat-marketplace/deploy/01-deploy-nft-marketplace.js
@@ -23,3 +23,5 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         log("------------------------------------------------------------");
     }
 }
+
+module.exports.tags = ["all", "nFTMarketplace"];
