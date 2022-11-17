676da27dfb79753c8c26bbc928fe78273097d8f0
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 16:25:23 +0200

    Add a script to move blocks
    This script will be used to move blocks in the tests.

---
+++ hardhat-marketplace/utils/moveBlocks.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat");
+
+const sleep = (timeInMs) => {
+    return new Promise((resolve) => setTimeout(resolve, timeInMs));
+}
+
+async function moveBlocks(amount, sleepAmount = 0) {
+    console.log("Moving blocks...");
+    for (let index = 0; index < amount; index++) {
+        await network.provider.request({
+            method: "evm_mine",
+            params: [],
+        });
+        if(sleepAmount) {
+            console.log(`Sleeping for ${sleepAmount}ms...`);
+            await sleep(sleepAmount);
+        }
+    }
+    console.log(`Moved ${amount} blocks`);
+}
+
+
+module.exports = {
+    moveBlocks,
+    sleep,
+};
--- nft-marketplace/cloudFunctions/updateActiveItems.js
+++ nft-marketplace/cloudFunctions/updateActiveItems.js
@@ -4,4 +4,8 @@ Moralis.Cloud.afterSave("ItemListed", async (request) => {
     const confirmed = request.object.get("confirmed");
     const logger = Moralis.Cloud.getLogger();
     logger.info("Looking for the confirmed TX items");
+    if(confirmed) {
+        logger.info("Found item!");
+
+    }
 });
