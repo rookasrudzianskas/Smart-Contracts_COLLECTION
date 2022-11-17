3d4c24aba23d9c4b8f2060ac421f1c2ed583c244
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 16:20:39 +0200

    Add a new feature
    This is a new feature that does something.

--- nft-marketplace/cloudFunctions/updateActiveItems.js
+++ nft-marketplace/cloudFunctions/updateActiveItems.js
@@ -1,5 +1,7 @@
 import Moralis from "moralis";
 
 Moralis.Cloud.afterSave("ItemListed", async (request) => {
-
+    const confirmed = request.object.get("confirmed");
+    const logger = Moralis.Cloud.getLogger();
+    logger.info("Looking for the confirmed TX items");
 });
