6dc27cef6048daa06ea09ade2b6d65552f395e96
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 17:43:38 +0200

    Add NFT Box component
    This component will be used to display the NFTs on the marketplace.

---
+++ nft-marketplace/components/nftBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NftBox = ({ price, nftAddress, tokenId, marketplaceAddress, seller }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NftBox;
+// by Rokas with ❤️
--- nft-marketplace/pages/index.tsx
+++ nft-marketplace/pages/index.tsx
@@ -5,34 +5,34 @@ import Header from '../components/Header';
 import {useMoralisQuery} from "react-moralis";
 
 const Home: NextPage = () => {
-  const {data: listedNfts, isFetching: fetchingListedNfts} = useMoralisQuery(
-      "ActiveItem",
+    const {data: listedNfts, isFetching: fetchingListedNfts} = useMoralisQuery(
+        "ActiveItem",
         (query) => query.limit(10).descending("tokenId"),
-  );
-  console.log(listedNfts);
-  return (
-    <div className="max-w-7xl mx-auto">
-      {fetchingListedNfts ? <p>Loading...</p> : (
-          <>
-            {listedNfts?.map((nft) => {
-              const { price, nftAddress, tokenId, marketplaceAddress, seller } = nft.attributes;
-              return (
-                  <div key={nft.id}>
-                    <p>{price}</p>
-                    <p>{nftAddress}</p>
-                    <Image
-                        src={seller}
-                        alt={seller}
-                        width={300}
-                        height={300}
-                    />
-                  </div>
-              )
-            })}
-          </>
-      )}
-    </div>
-  )
+    );
+    console.log(listedNfts);
+    return (
+        <div className="max-w-7xl mx-auto">
+            {fetchingListedNfts ? <p>Loading...</p> : (
+                <>
+                    {listedNfts?.map((nft) => {
+                        const { price, nftAddress, tokenId, marketplaceAddress, seller } = nft.attributes;
+                        return (
+                            <div key={nft.id}>
+                                <p>{price}</p>
+                                <p>{nftAddress}</p>
+                                <Image
+                                    src={seller}
+                                    alt={seller}
+                                    width={300}
+                                    height={300}
+                                />
+                            </div>
+                        )
+                    })}
+                </>
+            )}
+        </div>
+    )
 }
 
 export default Home
