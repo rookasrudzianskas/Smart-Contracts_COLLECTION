a6be77f210be2134c88024eab0b2a269becbfc9c
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 13:06:22 +0200

    Add border to header
    This is a test commit message

--- nft-marketplace/components/Header.js
+++ nft-marketplace/components/Header.js
@@ -4,14 +4,18 @@ import Link from "next/link";
 
 const Header = ({}) => {
     return (
-        <nav className="flex justify-between p-5">
+        <nav className="flex justify-between p-5 border-b-[2px]">
             <div>
                 <Link href={'/'}>
                     <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold md:font-bold">NFT Marketplace</h1>
                 </Link>
             </div>
-            <div>
-                <ConnectButton />
+            <div className="flex flex-row items-center space-x-3">
+                <Link className="text-sm font-semibold hover:bg-gray-200 py-1 px-2 rounded duration-150" href={'/'}>
+                    Home
+                </Link>
+                <Link className="text-sm font-semibold hover:bg-gray-200 py-1 px-2 rounded duration-150" href={'/sell-nft'}>Sell NFT</Link>
+                <ConnectButton moralisAuth={false} />
             </div>
         </nav>
     );
