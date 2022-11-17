65a056161aeda07035b57c95754910affc935d37
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 12:30:22 +0200

    Rokas

--- nft-marketplace/pages/index.tsx
+++ nft-marketplace/pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>NFT Marketplace</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      Hello!
+      <Header />
     </div>
   )
 }
