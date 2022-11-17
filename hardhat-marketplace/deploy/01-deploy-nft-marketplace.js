03fbb5ad6966ebb8bb5fdf9aae42aa0282821b6b
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 09:44:26 +0200

    Add NFTMarketplace contract
    This commit adds the NFTMarketplace contract to the project.
    
    The NFTMarketplace contract is a contract that allows users to create
    NFTs and sell them on the marketplace.
    
    The contract is deployed in the `01-deploy-nft-marketplace.js` script.
    
    The contract is verified on Etherscan.

--- hardhat-marketplace/deploy/01-deploy-nft-marketplace.js
+++ hardhat-marketplace/deploy/01-deploy-nft-marketplace.js
@@ -2,3 +2,24 @@ const { network } = require("hardhat");
 const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
 
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    let args = [];
+
+    const nftMarketplace = await deploy("NFTMarketplace", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || VERIFICATION_BLOCK_CONFIRMATIONS,
+    });
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log(`Verifying NFTMarketplace on Etherscan...`);
+        await verify(nftMarketplace.address, args);
+
+        log("Verified! ✅");
+        log("------------------------------------------------------------");
+    }
+}
