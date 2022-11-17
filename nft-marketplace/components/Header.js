566e10df6e8318a427c72ed1716fd30931dad2f6
Rokas <rookas.rudzenskas@gmail.com>
2022-11-17 12:38:35 +0200

    Add a link to the homepage
    The homepage is now a separate page, so we need to add a link to it.

--- nft-marketplace/components/Header.js
+++ nft-marketplace/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {ConnectButton} from "@web3uikit/web3";
+import Link from "next/link";
 
 const Header = ({}) => {
     return (
         <nav className="flex justify-between p-5">
             <div>
-                <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold md:font-bold">NFT Marketplace</h1>
+                <Link href={'/'}>
+                    <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold md:font-bold">NFT Marketplace</h1>
+                </Link>
             </div>
             <div>
                 <ConnectButton />
