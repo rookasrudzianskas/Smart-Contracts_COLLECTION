5822a69b2f591def9db946f3094ba65ce5cf9c3d
Rokas <rookas.rudzenskas@gmail.com>
2022-11-18 15:07:55 +0200

    Rokas

--- hardhat-dao-fcc/contracts/governance_standard/TimeLock.sol
+++ hardhat-dao-fcc/contracts/governance_standard/TimeLock.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 // Once the proposal submits, we will have to wait a little bit
 contract TimeLock is TimelockController {
+    // This contract, makes sure that the proposal is executed after a certain amount of time
     // minDelay is how long you have to wait before executing
     // proposers is the list of addresses that can propose
     // executors is the list of addresses that can execute
