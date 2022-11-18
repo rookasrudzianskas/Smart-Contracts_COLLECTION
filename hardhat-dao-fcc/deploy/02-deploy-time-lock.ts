89f7450af0969d2698597716e94e809da746cdbc
Rokas <rookas.rudzenskas@gmail.com>
2022-11-18 15:46:26 +0200

    Rokas

---
+++ hardhat-dao-fcc/deploy/02-deploy-time-lock.ts
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import verify from "../helper-functions"
+import { networkConfig, developmentChains, MIN_DELAY } from "../helper-hardhat-config"
+
+const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    // @ts-ignore
+    const {getNamedAccounts, deployments, network} = hre;
+    const {deploy, log} = deployments;
+    const {deployer} = await getNamedAccounts();
+    log("----------------------------------------------------");
+    log("Deploying TimeLock and waiting for confirmations...");
+
+}
+
+
+export default deployTimeLock;
+deployTimeLock.tags = ["all", "timelock"];
