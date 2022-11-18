bbee0de95af58c9451941e4634179c794609cee7
Rokas <rookas.rudzenskas@gmail.com>
2022-11-18 15:00:56 +0200

    Rokas

---
+++ hardhat-dao-fcc/contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+// Once the proposal submits, we will have to wait a little bit
+contract TimeLock is TimelockController {
+    // minDelay is how long you have to wait before executing
+    // proposers is the list of addresses that can propose
+    // executors is the list of addresses that can execute
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
